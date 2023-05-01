709b8fa912c1b8427a5ed22652fef991ab81d027
JoyceKyalo <107515318+JoyceKyalo@users.noreply.github.com>
2023-05-01 19:08:28 +0300

    Transferred weather details to its own component

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import UserDate from "./UserDate";
 import "./Weather.css";
 import axios from "axios";
+import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
   let [city, setCity] = useState(props.defaultCity);
-  let [weatherDetails, setWeatherDetails] = useState({ ready: false});
+  let [weatherDetails, setWeatherDetails] = useState({ ready: false });
 
   function updateCity(event) {
     setCity(event.target.value);
@@ -19,71 +19,41 @@ export default function Weather(props) {
   }
 
   function handleResponse(response) {
-   
     setWeatherDetails({
       ready: true,
       temperature: response.data.main.temp,
       description: response.data.weather[0].description,
       icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       date: new Date(response.data.dt * 1000),
-      city: response.data.name
+      city: response.data.name,
     });
   }
-  let searchForm = (
-    <div className="search-form">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="search"
-          placeholder="Enter a city"
-          autoFocus
-          onChange={updateCity}
-        />
-        <input type="submit" value="Search" />
-      </form>
-    </div>
-  );
 
   if (weatherDetails.ready) {
     return (
       <div>
-        <div className="row">
-          <div className="col-6">
-            <div className="row common-cities">
-              <div class="col-4">
-                <a href="/">New York</a>
-              </div>
-              <div class="col-3 paris-city">
-                <a href="/">Paris</a>
-              </div>
-              <div class="col-5">
-                <a href="/">Shanghai</a>
-              </div>
-            </div>
-            <UserDate date={weatherDetails.date} />
-            
+        <div>
+          <div className="search-form">
+            <form onSubmit={handleSubmit}>
+              <input
+                type="search"
+                placeholder="Enter a city"
+                autoFocus
+                onChange={updateCity}
+              />
+              <input type="submit" value="Search" />
+            </form>
           </div>
-          <div className="col-6">
-            <div>{searchForm}</div>
-            <ul className="current-weather-details">
-              <li>Temperature: {Math.round(weatherDetails.temperature)}°C</li>
-              <li>
-                Description:{" "}
-                <span id="weather-description">
-                  {weatherDetails.description}
-                </span>
-              </li>
-              <li>
-                <img src={weatherDetails.icon} alt={weatherDetails.icon} />
-              </li>
-            </ul>
-          </div>
+          
         </div>
+
+        <WeatherInfo details={weatherDetails} displayCity={props.defaultCity} />
       </div>
     );
   } else {
     let apiKey = "2d5c28f46f35496c26b3294dfcae8329";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
-    return <div>Loading</div>;
+    return <div>Loading...</div>;
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,38 @@
+import React from "react";
+import UserDate from "./UserDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-6">
+          <div className="row common-cities">
+            <div class="col-4">
+              <a href="/">New York</a>
+            </div>
+            <div class="col-3 paris-city">
+              <a href="/">Paris</a>
+            </div>
+            <div class="col-5">
+              <a href="/">Shanghai</a>
+            </div>
+          </div>
+          <h1>{props.displayCity}</h1>
+          <UserDate date={props.details.date} />
+        </div>
+        <div className="col-6">
+          <ul className="current-weather-details">
+            <li>Temperature: {Math.round(props.details.temperature)}°C</li>
+            <li>
+              Description:{" "}
+              <span id="weather-description">{props.details.description}</span>
+            </li>
+            <li>
+              <img src={props.details.icon} alt={props.details.icon} />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
