8ef529154962259a857c8b1e6015ceb90af92ea4
JoyceKyalo <107515318+JoyceKyalo@users.noreply.github.com>
2023-05-17 20:06:14 +0300

    Resized weather forecast icon

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,17 +1,17 @@
 import React from "react";
+import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
   return (
-    <div>
+    <div className="WeatherForecast">
       <div className="row">
         <div className="col">
           <div className="WeatherForecast-day">Thu</div>
           <div>
-            <img src={props.details.icon} alt={props.details.description} />
+            <img src={props.details.icon} alt={props.details.description} className="WeatherForecast-icon"/>
           </div>
           <div>
             <span>21°C</span>
-
             <span>19°C</span>
           </div>
         </div>
