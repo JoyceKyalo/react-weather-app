815065e48cfc734b30abeb5211bdc7e33f894b94
JoyceKyalo <107515318+JoyceKyalo@users.noreply.github.com>
2023-06-20 20:23:52 +0300

    Made API call for weather forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,18 @@
 import React from "react";
+import axios from "axios";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
+  let city = props.details.city;
+  let apiKey = "0534b4bf77df82f13f307a8bcbtddo1d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(handleResponse);
+
+  function handleResponse(response){
+    console.log(response.data);
+  }
+
   return (
     <div className="WeatherForecast">
       <div className="row">
