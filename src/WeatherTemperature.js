61dd9aa765f176be987cd03695c0e7353feff12b
JoyceKyalo <107515318+JoyceKyalo@users.noreply.github.com>
2023-06-23 00:41:37 +0300

    Removed celsius  to fahrenheit conversion and vice versa

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,39 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function fahrenheit() {
-    return (props.celsius * 9) / 5 + 32;
-  }
-  if (unit === "celsius") {
-    return (
-      <li>
-        Temperature: {Math.round(props.celsius)} °C |{" "}
-        <a href="/" onClick={showFahrenheit}>
-          °F
-        </a>
-      </li>
-    );
-  } else {
-    return (
-      <li>
-        Temperature: {Math.round(fahrenheit())}{" "}
-        <a href="/" onClick={showCelsius}>
-          °C
-        </a>{" "}
-        | °F
-      </li>
-    );
-  }
+  return <li>Temperature: {Math.round(props.celsius)} °C</li>;
 }
